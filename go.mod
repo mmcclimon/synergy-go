83be35e3894582d5d81277346fb7c4651dd43b26
Michael McClimon <michael@mcclimon.org>
2020-04-05 16:20:00 -0400

    slack: load users on startup

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/mmcclimon/synergy-go
 
 go 1.14
 
-require github.com/gorilla/websocket v1.4.2
+require (
+	github.com/gorilla/websocket v1.4.2
+	github.com/mitchellh/mapstructure v1.2.2
+)
--- internal/slack/client.go
+++ internal/slack/client.go
@@ -1,14 +1,18 @@
 package slack
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"sync"
+	"time"
 
 	"github.com/gorilla/websocket"
+	"github.com/mitchellh/mapstructure"
 )
 
 type arbitraryJSON = map[string]interface{}
@@ -22,7 +26,7 @@ type Client struct {
 	connected bool
 	isReady   bool
 	ws        *websocket.Conn
-	// websocket, plus internal data
+	usernames map[string]string
 }
 
 // NewClient gives you an instance of a slack client (not connected)
@@ -37,6 +41,7 @@ func NewClient() *Client {
 		apiKey:    apiKey,
 		connected: false,
 		isReady:   false,
+		usernames: make(map[string]string),
 	}
 
 	return &client
@@ -86,6 +91,17 @@ func (client *Client) connect() {
 
 	client.ws = conn
 	log.Println("connected to Slack!")
+
+	var wg sync.WaitGroup
+
+	wg.Add(3)
+	go client.loadUsers(&wg)
+	go client.loadChannels(&wg)
+	go client.loadDMs(&wg)
+	wg.Wait()
+
+	log.Println("loaded all the things!")
+	client.isReady = true
 }
 
 // Run is the central listen loop,
@@ -105,6 +121,7 @@ func (client *Client) Run() {
 		User    string
 	}
 
+	// is it bad form to reuse this? maybe!
 	var msg slackMessage
 
 	for {
@@ -120,6 +137,94 @@ func (client *Client) Run() {
 			continue
 		}
 
-		log.Printf("got message from user %s on channel %s: %s", msg.User, msg.Channel, msg.Text)
+		log.Printf("got message from user %s on channel %s: %s", client.usernameFor(msg.User), msg.Channel, msg.Text)
 	}
+}
+
+func (client *Client) usernameFor(id string) string {
+	username, ok := client.usernames[id]
+	if !ok {
+		log.Printf("no user found for id %s??", id)
+		return id
+	}
+
+	return username
+}
+
+func apiURL(method string) string {
+	return fmt.Sprintf("https://slack.com/api/%s", method)
+}
+
+func (client *Client) apiAuthHeader() string {
+	return fmt.Sprintf("Bearer %s", client.apiKey)
+}
+
+// probably this should also return an err, but will ignore for now
+func (client *Client) apiCall(endpoint string, postData arbitraryJSON) arbitraryJSON {
+	data, err := json.Marshal(postData)
+	if err != nil {
+		log.Fatal("couldn't encode json: ", err)
+	}
+
+	req, _ := http.NewRequest("POST", apiURL(endpoint), bytes.NewBuffer(data))
+	req.Header.Set("Authorization", client.apiAuthHeader())
+	req.Header.Set("Content-Type", "application/json")
+
+	res, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		log.Println("error talking to Slack:", err)
+	}
+
+	var ret arbitraryJSON
+	err = json.NewDecoder(res.Body).Decode(&ret)
+
+	if err != nil {
+		log.Fatal("invalid json from slack", err)
+	}
+
+	return ret
+}
+
+func (client *Client) loadUsers(wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	postData := make(arbitraryJSON)
+	postData["presence"] = false
+
+	data := client.apiCall("users.list", postData)
+
+	type user struct {
+		ID   string
+		Name string
+	}
+
+	var users []user
+
+	err := mapstructure.Decode(data["members"], &users)
+	if err != nil {
+		log.Println("error loading users", err)
+		return
+	}
+
+	for _, user := range users {
+		client.usernames[user.ID] = user.Name
+	}
+
+	// fix up our own data
+	client.usernames[client.ourID] = client.ourName
+
+	log.Println("loaded users")
+}
+
+func (client *Client) loadChannels(wg *sync.WaitGroup) {
+	defer wg.Done()
+	time.Sleep(2 * time.Second)
+	log.Println("loaded channels")
+}
+
+func (client *Client) loadDMs(wg *sync.WaitGroup) {
+	defer wg.Done()
+	time.Sleep(1 * time.Second)
+	log.Println("loaded DMs")
 }
